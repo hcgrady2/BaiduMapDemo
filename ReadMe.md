7db01d30ff839879f156d545fcb3368554122485
hc <hc@hcs-MacBook-Pro.local>
2020-02-18 15:54:01 +0800

    百度地图类型及定位

--- ReadMe.md
+++ ReadMe.md
@@ -56,7 +56,7 @@ mBaiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
 
 
 #### 地图容器介绍
-百度地图 SDK 地图容器有两周： GLSurfaceView 和  TextureView,都是 OpenGL ES 组件。
+百度地图 SDK 地图容器有两种： GLSurfaceView 和  TextureView,都是 OpenGL ES 组件。
 
 .GLSurfaceView:
 包括MapView，MapFragment和SupportMapFragment三种容器。
@@ -72,5 +72,134 @@ MapFragment需要在android3.0以上的版本中使用。
 
 
 
+#### 百度地图类型
+地图SDK提供了3种预置的地图类型，包括普通地图，卫星图，空白地图。另外提供了2种常用图层实时路况图以及百度城市热力图。
+
+其中空白地图：
+无地图瓦片,地图将渲染为空白地图。不加载任何图块，将不会使用流量下载基础地图瓦片图层。支持叠加任何覆盖物。
+
+适用场景:与瓦片图层（tileOverlay）一起使用，节省流量，提升自定义瓦片图下载速度。
 
 
+
+##### 预置类型使用
+```
+mMapView = (MapView) findViewById(R.id.bmapView);  
+mBaiduMap = mMapView.getMap();  
+//普通地图 ,mBaiduMap是地图控制器对象
+mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);   
+
+//卫星地图   
+mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);  
+
+
+//空白地图 
+mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NONE);
+
+
+```
+
+##### 路况图和热力图
+```
+
+mMapView = (MapView) findViewById(R.id.bmapView); 
+mBaiduMap = mMapView.getMap();  
+//开启交通图   
+mBaiduMap.setTrafficEnabled(true);
+
+//自定义颜色
+mBaiduMap.setTrafficEnabled(true);
+mBaiduMap.setCustomTrafficColor("#ffba0101", "#fff33131", "#ffff9e19", "#00000000");
+//  对地图状态做更新，否则可能不会触发渲染，造成样式定义无法立即生效。
+MapStatusUpdate u = MapStatusUpdateFactory.zoomTo(13);
+mBaiduMap.animateMapStatus(u);
+
+
+
+mMapView = (MapView) findViewById(R.id.bmapView); 
+mBaiduMap = mMapView.getMap();  
+//开启热力图  
+mBaiduMap.setBaiduHeatMapEnabled(true);
+
+```
+
+#### 百度地图中的定位显示
+在国内获得的坐标系类型可以是：国测局坐标、百度墨卡托坐标 和 百度经纬度坐标。
+1、需要在 manifest 中声明定位权限，和定位 Service
+2、开启地图定位图层
+```
+mBaiduMap.setMyLocationEnabled(true);
+
+```
+3、我们通过继承抽象类BDAbstractListener并重写其onReceieveLocation方法来获取定位数据，并将其传给MapView。
+```
+ublic class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+    }
+}
+```
+
+4、通过LocationClient发起定位
+也就是，locationClient 回开始定位，通过自己写的内部类回调获取定位信息，并传递给 BaidMap
+```
+//定位初始化
+mLocationClient = new LocationClient(this);
+
+//通过LocationClientOption设置LocationClient相关参数
+LocationClientOption option = new LocationClientOption();
+option.setOpenGps(true); // 打开gps
+option.setCoorType("bd09ll"); // 设置坐标类型
+option.setScanSpan(1000);
+
+//设置locationClientOption
+mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+MyLocationListener myLocationListener = new MyLocationListener();
+mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+mLocationClient.start();
+
+```
+
+5、正确管理生命周期
+```
+@Override
+protected void onResume() {
+    mMapView.onResume();
+    super.onResume();
+}
+
+@Override
+protected void onPause() {
+    mMapView.onPause();
+    super.onPause();
+}
+
+@Override
+protected void onDestroy() {
+    mLocationClient.stop();
+    mBaiduMap.setMyLocationEnabled(false);
+    mMapView.onDestroy();
+    mMapView = null;
+    super.onDestroy();
+}
+```
+6、自定义定位有关内容
+通过MyLocationConfiguration类来构造包括定位的属性，定位模式、是否开启方向、设置自定义定位图标、精度圈填充颜色以及精度圈边框颜色5个属性。
+
+```
+mBaiduMap.setMyLocationConfiguration(mLocationConfiguration)
+
+```
--- app/src/main/java/com/hc/mapdemo/MainActivity.java
+++ app/src/main/java/com/hc/mapdemo/MainActivity.java
@@ -2,26 +2,71 @@ package com.hc.mapdemo;
 
 
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.LogoPosition;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.SupportMapFragment;
 import com.baidu.mapapi.map.TextureSupportMapFragment;
 import com.baidu.mapapi.model.LatLng;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks,
+        EasyPermissions.RationaleCallbacks{
+
+
+    //动态权限申请
+    private static final String[] LOCATION_AND_CAMERA=
+            {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA};
+
+    private static final int RC_CAMERA_PERM = 123;
+    private static final int RC_LOCATION_CAMERA_PERM = 124;
+
+
+
+
+    private static final String TAG = "BaiduMapDemo";
     private MapView mMapView = null;
+    private  BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       // locationAndCameraTask();
+        
+        
 
 
         /** 最基本的 layout 使用方式，直接在 layout 中使用 MapView 布局
@@ -31,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
        // MapStatus.Builder builder = new MapStatus.Builder();
        // builder.zoom(21.0f);
         //mMapView.getMap().setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+
+
 
 
         /**
@@ -46,42 +93,130 @@ public class MainActivity extends AppCompatActivity {
 //
 
 
+
+
         /**
          * 多实例显示地图
          */
-        LatLng GEO_BEIJING = new LatLng(39.945, 116.404);
-        LatLng GEO_SHANGHAI = new LatLng(31.227, 121.481);
+//        LatLng GEO_BEIJING = new LatLng(39.945, 116.404);
+//        LatLng GEO_SHANGHAI = new LatLng(31.227, 121.481);
+//
+//        //北京为地图中心，logo在左上角
+//        MapStatusUpdate status1 = MapStatusUpdateFactory.newLatLng(GEO_BEIJING);
+//        SupportMapFragment map1 = (SupportMapFragment) (getSupportFragmentManager()
+//                .findFragmentById(R.id.map1));
+//        map1.getBaiduMap().setMapStatus(status1);
+//        map1.getMapView().setLogoPosition(LogoPosition.logoPostionleftTop);
+//
+//        //上海为地图中心
+//        MapStatusUpdate status2 = MapStatusUpdateFactory.newLatLng(GEO_SHANGHAI);
+//        SupportMapFragment map2 = (SupportMapFragment) (getSupportFragmentManager()
+//                .findFragmentById(R.id.map2));
+//        map2.getBaiduMap().setMapStatus(status2);
 
-        //北京为地图中心，logo在左上角
-        MapStatusUpdate status1 = MapStatusUpdateFactory.newLatLng(GEO_BEIJING);
-        SupportMapFragment map1 = (SupportMapFragment) (getSupportFragmentManager()
-                .findFragmentById(R.id.map1));
-        map1.getBaiduMap().setMapStatus(status1);
-        map1.getMapView().setLogoPosition(LogoPosition.logoPostionleftTop);
 
-        //上海为地图中心
-        MapStatusUpdate status2 = MapStatusUpdateFactory.newLatLng(GEO_SHANGHAI);
-        SupportMapFragment map2 = (SupportMapFragment) (getSupportFragmentManager()
-                .findFragmentById(R.id.map2));
-        map2.getBaiduMap().setMapStatus(status2);
 
+
+
+
+        mMapView = (MapView) findViewById(R.id.bmapView);
+         mBaiduMap = mMapView.getMap();
+        //普通地图 ,mBaiduMap是地图控制器对象
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //开启交通图
+        mBaiduMap.setTrafficEnabled(true);
+
+
+//        mBaiduMap.setTrafficEnabled(true);
+//        mBaiduMap.setCustomTrafficColor("#ffba0101", "#ffba0101", "#ffba0101", "#ffba0101");
+////  对地图状态做更新，否则可能不会触发渲染，造成样式定义无法立即生效。
+//        MapStatusUpdate u = MapStatusUpdateFactory.zoomTo(13);
+//        mBaiduMap.animateMapStatus(u);
+
+
+
+        //开启热力图
+      //  mBaiduMap.setBaiduHeatMapEnabled(true);
+
+        mBaiduMap.setMyLocationEnabled(true);
+
+
+        Log.i(TAG, "onCreate: 准备开始定位");
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+    //    mLocationClient.start();
+
+
+
+        //自定义定位属性
+        MyLocationConfiguration.LocationMode mode = MyLocationConfiguration.LocationMode.FOLLOWING;
+        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_background);
+        int accuracyCircleFillColor = 0xAAFFFF88;
+        int accuracyCircleStrokeColor = 0xAA00FF00;
+        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(
+                mode,true,mCurrentMarker,accuracyCircleFillColor,accuracyCircleStrokeColor
+        ));
+
+
+
+
+        mBaiduMap.setIndoorEnable(true);//打开室内图，默认为关闭状态
 
 
 
 
 
     }
+
+
+    @AfterPermissionGranted(RC_LOCATION_CAMERA_PERM)
+    private void locationAndCameraTask() {
+
+        if (hasLocationAndCamerasPermissions()) {
+            // Have permissions, do the thing!
+            Toast.makeText(this, "TODO: Location and Contacts things", Toast.LENGTH_LONG).show();
+        } else {
+            // Ask for both permissions
+            EasyPermissions.requestPermissions(
+                    this,
+                    "我们需要定位权限",
+                    RC_LOCATION_CAMERA_PERM,
+                    LOCATION_AND_CAMERA);
+        }
+        
+        
+    }
+
+    private boolean hasLocationAndCamerasPermissions() {
+        return EasyPermissions.hasPermissions(this, LOCATION_AND_CAMERA);
+
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-      //  mMapView.onResume();
+        mMapView.onResume();
     }
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        //mMapView.onPause();
+        mMapView.onPause();
     }
     @Override
     protected void onDestroy() {
@@ -89,4 +224,100 @@ public class MainActivity extends AppCompatActivity {
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
        // mMapView.onDestroy();
     }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+        Log.i(TAG, "onPermissionsGranted: 获取定位权限");
+
+
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+//        mLocationClient.start();
+
+
+
+
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+
+    }
+
+    @Override
+    public void onRationaleAccepted(int requestCode) {
+
+    }
+
+    @Override
+    public void onRationaleDenied(int requestCode) {
+
+    }
+
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+
+
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+
+            Log.i(TAG, "onReceiveLocation: 收到了定位信息:" + location.getAddrStr());
+
+
+            mBaiduMap.setMyLocationData(locData);
+        }
+    }
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+
+
 }
+
